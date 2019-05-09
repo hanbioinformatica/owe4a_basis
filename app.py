5d6c70eafcff82fe0ee39791d986536c016c1b91
martijn <Martijn.vanderBruggen@han.nl>
2019-05-09 10:17:31 +0200

    addedRequirements

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+"""
+Basis voor een te ontwikkelen Flask applicatie
+Creation: may 9, 2019
+(c) HAN University of Applied Science
+Author: Martijn van der Bruggen
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def index_fun():
+    return 'Dit is de routing naar de root'
 
 if __name__ == '__main__':
     app.run()
