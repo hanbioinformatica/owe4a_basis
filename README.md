5e084689a420cb35951d58ce423c3b65a8ed3f53
martijn <Martijn.vanderBruggen@han.nl>
2019-05-09 10:42:02 +0200

    Cleaning code

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # owe4a_basis
 
 Basisopzet van een Flask applicatie
-Dit PyCharm project is te gebruiken als uitgangspunt voor een Flask Applicatie
+Dit PyCharm project is te gebruiken als uitgangspunt 
+voor een Flask Applicatie.
+
+* requirements.txt bevat de belangrijkste requirements. Door deze op te nemen in de requirements file worden ze automatisch geinstalleerd bij deployment naar Azure.
+* bestand met de naam application.py. Hierdoor wordt het in de webapp herkend als de uit te voeren app
+  
--- application.py
+++ application.py
@@ -7,7 +7,6 @@ Author: Martijn van der Bruggen
 Voor deployment op Azure WebApps is het noodzakelijk het bestand
 de naam application.py te geven.
 """
-import mysql.connector
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -16,36 +15,6 @@ app = Flask(__name__)
 @app.route('/')
 def index_fun():
     return 'Dit is de routing naar de root'
-
-
-"""
-Mogelijkheid om te zoeken naar een woord in de ensembl database
-Parameteroverdracht middels de get methode (default)
-"""
-
-
-@app.route("/sql")
-def sqldemo():
-    woord = request.args.get('woord')
-    if woord == None: woord = "koe"
-    verbinding = mysql.connector.connect(host="ensembldb.ensembl.org",
-                                         user="anonymous",
-                                         db="homo_sapiens_core_95_38")
-    cursor = verbinding.cursor()
-    cursor.execute("select * from gene where description like '%{}%' limit 10".format(woord))
-    regel = ""
-    tekst = """<form method="get">
-                <input type="text" name="woord">
-                <input type="submit" value="Submit">
-                </form>
-                <hr>"""
-    while regel != None:
-        if len(regel) > 9:
-            tekst += str(regel[9]).replace(woord, "<b>" + woord + "</b>") + "<br>"
-        regel = cursor.fetchone()
-    cursor.close()
-    verbinding.close()
-    return tekst
 
 
 if __name__ == '__main__':
